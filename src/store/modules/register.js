14e1fadeddcd54f39e7185e9e668985e1c334cae
Abhilash Kulkarni <abhilashkulkarniofficial@gmail.com>
2021-06-16 21:12:07 +0530

    Testing the server

--- src/store/modules/register.js
+++ src/store/modules/register.js
@@ -20,6 +20,7 @@ const actions = {
     let result
     try{
       let body = userData.body
+      body['test'] = true
       await axios.post(`${process.env.VUE_APP_SERVER}/newUser`,body)
         .then(response => {
           // console.log(response)
