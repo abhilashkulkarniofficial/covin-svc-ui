577a76e5ac4b6df1484eefa998de364fce00b65f
Abhilash Kulkarni <abhilashkulkarniofficial@gmail.com>
2021-06-17 11:13:05 +0530

    Fixed the problem temporarily

--- src/store/modules/search.js
+++ src/store/modules/search.js
@@ -53,9 +53,49 @@ const actions = {
     commit('setDistricts', result.data.districts)
   },
   async searchCenters({commit}, filter){
-    let result = await axios.get(`${process.env.VUE_APP_SERVER}/getCenters`,{params:filter})
+    if(!filter.vaccinePref || !filter.vaccinePref.length){
+        filter.vaccinePref = [ 'COVAXIN', 'COVISHIELD', 'SPUTNIK' ]
+    }
+
+    if(filter.ageLimit){
+      filter.ageLimit = [parseInt(filter.ageLimit)]
+    }else{
+        filter.ageLimit = [0, 18, 45]
+    }
+    let query = ``
+    let params = {}
+    if(filter.searchBy[1]){
+      query = `${query}https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin`
+      params['date'] = filter.date
+      params['pincode'] = filter.pincode
+      // result = await axios.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin',{params:filter})
+    }else{
+      query = `${query}https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict`
+      params['date'] = filter.date
+      params['district_id'] = filter.district_id
+      // result = await axios.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict',{params:filter})
+    }
+    await axios.get(query, {params:params})
+        .then(response => {
+            // response.data.sessions = parseCenters(filter, response)
+            let sessions = []
+            for(let i=0; i<response.data.sessions.length; i++){
+                if(response.data.sessions[i].available_capacity>0 
+                    && filter.vaccinePref.includes(response.data.sessions[i].vaccine) 
+                    && filter.ageLimit.includes(response.data.sessions[i].min_age_limit)){
+                    response.data.sessions[i]['reveal'] = false
+                    response.data.sessions[i]['moreDetails'] = false
+                    sessions.push(response.data.sessions[i])
+                }
+            }
+            response.data.sessions = sessions
+            commit('setSlots', response.data.sessions)
+            // res.status(200).send(response.data)
+        })
+
+    // let result = await axios.get(`${process.env.VUE_APP_SERVER}/getCenters`,{params:filter})
     // console.log(result.data)
-    commit('setSlots', result.data.sessions)
+    
   
     // commit('setPincode', filter.pincode)
   }
